446ed2087cc41114d031c9da1b2d2fc9103262c5
Your Name <you@example.com>
2023-02-27 22:25:34 +0900

    Add church numeral syntax sugar

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Control.Monad
 import Control.Monad.Combinators
 import Data.List
 import Data.Text (Text)
@@ -31,7 +30,11 @@ main :: IO ()
 main =
   T.getContents >>= \input -> case parse (expr <* eof) "slambda" input of
     Left e -> putStrLn $ errorBundlePretty e
-    Right x -> forM_ (steps x) print
+    Right x -> do
+      mapM_ (\y -> putStrLn $ "= " ++ show y) (steps x)
+      mapM_
+        (\n -> putStrLn $ show n ++ " (nat)")
+        (fromChurchNum $ last $ steps x)
 
 steps :: Ast -> [Ast]
 steps x = unfoldr steps' (Just x)
@@ -57,6 +60,22 @@ subst old new (Abs x m) = Abs x' (subst old new m')
     m' = subst x (Var x') m
 subst old new (App m n) = App (subst old new m) (subst old new n)
 
+fromChurchNum :: Ast -> Maybe Int
+fromChurchNum (Abs f (Abs x n)) = fromChurchNum' n
+  where
+    fromChurchNum' (Var x')
+      | x' == x = Just 0
+    fromChurchNum' (App (Var m) n')
+      | m == f = fmap (+ 1) (fromChurchNum' n')
+    fromChurchNum' _ = Nothing
+fromChurchNum _ = Nothing
+
+toChurchNum :: Int -> Ast
+toChurchNum n = Abs "f" (Abs "x" (toChurchNum' n))
+  where
+    toChurchNum' 0 = Var "x"
+    toChurchNum' m = App (Var "f") (toChurchNum' (m - 1))
+
 type Parser = Parsec Void Text
 
 -- expr ::= abs | app
@@ -74,10 +93,11 @@ abs_ =
 app :: Parser Ast
 app = foldl1 App <$> some factor
 
--- factor ::= var | "(" expr ")"
+-- factor ::= var | nat | "(" expr ")"
 factor :: Parser Ast
 factor =
   ws *> var <* ws
+    <|> ws *> nat <* ws
     <|> ws *> char '(' *> expr <* char ')' <* ws
 
 -- var ::= ident
@@ -87,6 +107,10 @@ var = Var <$> ident
 -- ident ::= [a-zA-Z] [0-9a-zA-Z]*
 ident :: Parser String
 ident = (:) <$> letterChar <*> many alphaNumChar <?> "identifier"
+
+-- nat ::= [0-9]*
+nat :: Parser Ast
+nat = toChurchNum . read <$> some digitChar
 
 ws :: Parser ()
 ws = hidden space
