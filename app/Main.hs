65c1776b99572b83f482d4a5f1d37a7905db6e5a
Your Name <you@example.com>
2023-03-14 03:09:34 +0900

    Add let in expression

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,12 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
-type Let = (String, Ast)
-
 data Ast
   = Var String
   | Abs String Ast
   | App Ast Ast
+  | Nat Int
+  | LetIn String Ast Ast
   deriving (Eq)
 
 instance Show Ast where
@@ -25,15 +25,17 @@ instance Show Ast where
         _ -> show m
       sn = case n of
         (Var _) -> show n
+        (Nat _) -> show n
         _ -> "(" ++ show n ++ ")"
+  show (Nat n) = show n
+  show (LetIn x m n) = "let " ++ x ++ " = " ++ show m ++ " in\n" ++ show n
 
 main :: IO ()
 main =
   getContents >>= \input -> case parse slambda "slambda" input of
     Left e -> putStrLn $ errorBundlePretty e
-    Right (lets, x) -> do
-      -- TODO: Substitution in let in expressions.
-      let st = steps $ foldr (\(k, v) x' -> subst [] k v x') x lets
+    Right x -> do
+      let st = steps x
       mapM_ (\y -> putStrLn $ "= " ++ show y) st
       mapM_
         (\n -> putStrLn $ show n ++ " (nat)")
@@ -53,7 +55,10 @@ step = step' []
     step' _ (Var x) = Var x
     step' vars (Abs x m) = Abs x (step' (x : vars) m)
     step' vars (App (Abs x m) n) = subst vars x n m
+    step' vars (App (Nat n) m) = App (alphaConv vars $ toChurchNum n) m
     step' vars (App m n) = App (step' vars m) (step' vars n)
+    step' _ (Nat n) = Nat n
+    step' vars (LetIn x m n) = subst vars x m n
 
 subst :: [String] -> String -> Ast -> Ast -> Ast
 subst vars old new (Var x)
@@ -62,6 +67,9 @@ subst vars old new (Var x)
 subst vars old new (Abs x m) =
   alphaConv vars $ Abs x (subst (x : vars) old new m)
 subst vars old new (App m n) = App (subst vars old new m) (subst vars old new n)
+subst _ _ _ (Nat n) = Nat n
+subst vars old new (LetIn x m n) =
+  LetIn x (subst vars old new m) (subst vars old new n)
 
 alphaConv :: [String] -> Ast -> Ast
 alphaConv _ (Var x) = Var x
@@ -69,6 +77,8 @@ alphaConv vars (Abs x m) = Abs x' (subst vars x (Var x') m)
   where
     x' = freshName vars x
 alphaConv vars (App m n) = App (alphaConv vars m) (alphaConv vars n)
+alphaConv _ (Nat n) = Nat n
+alphaConv vars (LetIn x m n) = LetIn x (alphaConv vars m) (alphaConv vars n)
 
 freshName :: [String] -> String -> String
 freshName vars x
@@ -86,10 +96,10 @@ fromChurchNum (Abs f (Abs x n)) = fromChurchNum' n
 fromChurchNum _ = Nothing
 
 toChurchNum :: Int -> Ast
-toChurchNum n = Abs "f" (Abs "x" (toChurchNum' n))
+toChurchNum n = Abs "s" (Abs "z" (toChurchNum' n))
   where
-    toChurchNum' 0 = Var "x"
-    toChurchNum' m = App (Var "f") (toChurchNum' (m - 1))
+    toChurchNum' 0 = Var "z"
+    toChurchNum' m = App (Var "s") (toChurchNum' (m - 1))
 
 type Parser = Parsec Void String
 
@@ -97,16 +107,12 @@ kws :: [String]
 kws = ["let", "in"]
 
 -- slambda ::= letIn* expr
-slambda :: Parser ([Let], Ast)
-slambda = ((,) <$> many letIn <*> expr) <* eof
+slambda :: Parser Ast
+slambda = expr <* eof
 
--- letIn ::= "let" ident "=" expr "in"
-letIn :: Parser Let
-letIn = (,) <$> (symbol "let" *> ident) <*> (symbol "=" *> expr <* symbol "in")
-
--- expr ::= abs | app
+-- expr ::= abs | app | letIn
 expr :: Parser Ast
-expr = abs_ <|> app
+expr = abs_ <|> app <|> letIn
 
 -- abs ::= [\\^λ] ident "." expr
 abs_ :: Parser Ast
@@ -118,6 +124,14 @@ abs_ =
 -- app ::= factor+
 app :: Parser Ast
 app = foldl1 App <$> some factor
+
+-- letIn ::= "let" ident "=" expr "in" expr
+letIn :: Parser Ast
+letIn =
+  LetIn
+    <$> (symbol "let" *> ident)
+    <*> (symbol "=" *> expr)
+    <*> (symbol "in" *> expr)
 
 -- factor ::= var | nat | "(" expr ")"
 factor :: Parser Ast
@@ -137,7 +151,7 @@ ident = lexeme (p >>= check)
 
 -- nat ::= [0-9]*
 nat :: Parser Ast
-nat = toChurchNum <$> lexeme L.decimal
+nat = Nat <$> lexeme L.decimal
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
